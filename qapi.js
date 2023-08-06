7795106ac055dc5e4ddcd66ef0f23302612b1be8
Ahmed Dahab <38168753+aydahab@users.noreply.github.com>
2023-08-06 23:32:11 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"64d0006a6efcea001ae4dc94",
+				"64d0006a6efcea001ae4dc97"
+			]
+		},
+		"64d0006a6efcea001ae4dc94": {
+			"id": "64d0006a6efcea001ae4dc94",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"64d0006a6efcea001ae4dc97": {
+			"id": "64d0006a6efcea001ae4dc97",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
